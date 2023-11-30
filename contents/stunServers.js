10699be2a7de7d7df107f93105e58d63b9ab7673
Seven Yu <dofyyu@gmail.com>
2023-11-30 21:30:01 +0800

    move content outside

---
+++ contents/stunServers.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    id: "google",
+    name: "Google",
+    url: "stun:stun.l.google.com:19302",
+    ip: "",
+  },
+  {
+    id: "nextcloud",
+    name: "NxtCld",
+    url: "stun:stun.nextcloud.com:443",
+    ip: "",
+  },
+  {
+    id: "peerjs",
+    name: "PeerJS",
+    url: "stun:us-0.turn.peerjs.com",
+    ip: "",
+  },
+  {
+    id: "twilio",
+    name: "Twilio",
+    url: "stun:global.stun.twilio.com",
+    ip: "",
+  },
+  {
+    id: "cloudflare",
+    name: "Cloudflare",
+    url: "stun:stun.cloudflare.com",
+    ip: "",
+  },
+  {
+    id: "miwifi",
+    name: "MiWiFi",
+    url: "stun:stun.miwifi.com",
+    ip: "",
+  },
+  {
+    id: "qq",
+    name: "QQ",
+    url: "stun:stun.qq.com",
+    ip: "",
+  },
+  {
+    id: "stunprotocol",
+    name: "StnPtc",
+    url: "stun:stunserver.stunprotocol.org",
+    ip: "",
+  },
+];
