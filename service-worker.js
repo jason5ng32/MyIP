0935fd1ed23c8af91af5ed16b4dd02d32abd095f
Jeamorg <93057377+Jeamorg@users.noreply.github.com>
2023-12-02 22:59:17 +0800

    Use cloudflare cdn

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,9 @@ const CACHE_NAME = 'v1_cache';
 const urlsToCache = [
     'res/vue.js',
     'res/defaultMap.jpg',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
-    'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.2/font/bootstrap-icons.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css',
 
 ];
