6097a15f8d3f2f0dd30b62c5041680b965da59b2
jason5ng32 <jason5ng32@gmail.com>
2023-12-08 12:51:47 +0800

    Improve GA

--- res/ga.js
+++ res/ga.js
@@ -1,15 +1,5 @@
-let googleAnalyticsId;
-
-try {
-  // Read the environment variable
-  googleAnalyticsId = process.env.VUE_APP_GOOGLE_ANALYTICS_ID || 'GA-ID-Here';
-} catch (e) {
-  // If it's not defined, use the default value
-  googleAnalyticsId = 'GA-ID-Here';
-}
-
 const config = {
-  GOOGLE_ANALYTICS_ID: googleAnalyticsId
+    GOOGLE_ANALYTICS_ID: 'G-TEYKKD81TL'
 };
 
 export default config;
