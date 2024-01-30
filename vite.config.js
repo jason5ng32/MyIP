3cf467d9574218c39145219dba7639cd4f02cf2a
jason5ng32 <jason5ng32@gmail.com>
2024-01-30 10:22:45 +0800

    PWA Improvements

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,42 @@
 import dotenv from 'dotenv';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 dotenv.config();
 
 const apiPort = process.env.PORT || 11966;
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}']
+      },
+      manifest: {
+        name: 'IPCheck.ing',
+        short_name: 'IPCheck.ing',
+        theme_color: '#f8f9fa',
+        orientation: "portrait",
+        id: 'com.jasonng.myip',
+        icons: [
+          {
+            src: '/logo-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/logo-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
