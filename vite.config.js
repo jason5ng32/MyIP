272321c0e9aefdc4b6dae0964a738965710aeea1
jason5ng32 <jason5ng32@gmail.com>
2024-04-01 10:41:55 +0800

    Improvements

--- vite.config.js
+++ vite.config.js
@@ -38,11 +38,17 @@ export default defineConfig({
             src: '/logo-192.webp',
             sizes: '192x192',
             type: 'image/webp',
+            purpose: 'maskable'
           },
           {
-            src: '/logo-512.webp',
+            src: '/ios-logo-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/ios-logo-512.png',
             sizes: '512x512',
-            type: 'image/webp',
+            type: 'image/png',
           },
         ],
       },
