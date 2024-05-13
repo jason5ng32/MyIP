d2b92577c45f08615a2d3e7599bce5d87959cc10
jason5ng32 <jason5ng32@gmail.com>
2024-05-13 19:51:31 +0800

    V4.0

--- api/invisibilitytest.js
+++ api/invisibilitytest.js
@@ -14,6 +14,11 @@ function isValidUserID(userID) {
     return true;
 }
 
+function isSafeURL(url) {
+    const allowedHost = 'api.ipcheck.ing';
+    return url.hostname === allowedHost;
+}
+
 export default (req, res) => {
 
     // 限制只能从指定域名访问
@@ -39,6 +44,10 @@ export default (req, res) => {
     }
 
     const url = new URL(`https://api.ipcheck.ing/getpdresult/${id}?apikey=${apikey}`);
+
+    if (!isSafeURL(url)) {
+        return res.status(400).json({ error: 'Unsafe URL detected' });
+    }
 
     get(url, apiRes => {
         let data = '';
