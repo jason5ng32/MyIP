ae82cf99fe5e7b635d3d01473d01f6a9c56d10d1
jason5ng32 <jason5ng32@gmail.com>
2024-05-13 19:40:56 +0800

    V4.0

--- api/invisibilitytest.js
+++ api/invisibilitytest.js
@@ -33,6 +33,11 @@ export default (req, res) => {
     }
 
     const apikey = process.env.IPCHECKING_API_KEY;
+
+    if (!apikey) {
+        return res.status(500).json({ error: 'API key is missing' });
+    }
+
     const url = new URL(`https://api.ipcheck.ing/getpdresult/${id}?apikey=${apikey}`);
 
     get(url, apiRes => {
